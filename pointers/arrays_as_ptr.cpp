6ace75b0c712f342b5efece959357ba2e45acd71
Said Nabi <117791475+Nabisaeed@users.noreply.github.com>
2024-02-05 11:42:03 +0500

    Lecture 07 -  5-feb 2024

---
+++ pointers/arrays_as_ptr.cpp
@@ -0,0 +1,16 @@
+// C++ Program starts here
+#include <iostream>
+using namespace std;
+    
+    
+int main()
+{
+    int arr[5]={1,12,25,7,11};
+
+    int * first=arr;  
+
+    cout<<*first+2<<endl;
+    
+     return 0;
+    
+}
